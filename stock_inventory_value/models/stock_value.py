dad708bceb10f86a26e2f5f01f5f7c54acf79d8b
Sergio Díaz <sergiodm.1989@gmail.com>
2019-11-27 23:05:30 +0100

    [WIP] stock_inventory_value: Cron to compute stock value

--- stock_inventory_value/models/stock_value.py
+++ stock_inventory_value/models/stock_value.py
@@ -3,6 +3,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models, fields, _
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class StockValue(models.Model):
@@ -14,6 +17,17 @@ class StockValue(models.Model):
 
     @api.model
     def _stock_value_process(self):
-        # TODO
-        print("_stock_value_process...")
+        _logger.info("@Stock value cron: Start process")
+        products = self.env['product.product'].search([
+            ('type', 'in', ['consu', 'product'])])
+        stock_value = 0
+        for product in products:
+            _logger.info("@Stock value cron: Computing total value for the "
+                         "product [%s]" % product.name)
+            stock_value += product.qty_available * product.standard_price
+        _logger.info("@Stock value cron: Creating stock.value entry...")
+        self.create({
+            'date': fields.Datetime.now(),
+            'stock_value': stock_value})
+        _logger.info("@Stock value cron: End process.")
         return
