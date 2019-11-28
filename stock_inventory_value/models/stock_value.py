87f159825a4fe5db7e6974614b51680f699e0560
Sergio Díaz <sergiodm.1989@gmail.com>
2019-11-28 23:02:30 +0100

    [ADD] stock_inventory_value: Cron to compute stock value

--- stock_inventory_value/models/stock_value.py
+++ stock_inventory_value/models/stock_value.py
@@ -12,19 +12,31 @@ class StockValue(models.Model):
     _name = 'stock.value'
 
     date = fields.Datetime(string="Date", required=True, readonly=True)
-    stock_value = fields.Float(
-        string="Stock value", required=True, readonly=True)
+    stock_value = fields.Monetary(
+        string="Stock value", required=True, readonly=True,
+        currency_field='currency_id')
+    currency_id = fields.Many2one(
+        'res.currency', string='Currency',
+        default=lambda self: self.env.user.company_id.currency_id.id)
 
     @api.model
     def _stock_value_process(self):
         _logger.info("@Stock value cron: Start process")
-        products = self.env['product.product'].search([
-            ('type', 'in', ['consu', 'product'])])
+        quant_obj = self.env['stock.quant']
+        product_domain = [('type', '=', 'product')]
+        products = self.env['product.product'].search(product_domain)
+        cont = 0
         stock_value = 0
         for product in products:
+            cont += 1
             _logger.info("@Stock value cron: Computing total value for the "
-                         "product [%s]" % product.name)
-            stock_value += product.qty_available * product.standard_price
+                         "product [%s]. Product %s of %s." % (
+                product.name, cont, len(products)))
+            quant_domain = [
+                ('product_id', '=', product.id),
+                ('location_id.usage', '=', 'internal')]
+            quants = quant_obj.search(quant_domain)
+            stock_value += sum(quants.mapped('inventory_value'))
         _logger.info("@Stock value cron: Creating stock.value entry...")
         self.create({
             'date': fields.Datetime.now(),
