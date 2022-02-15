aeaae7c1b345b71223d95bae37b60818dea3fed0
onduex <tomas.pascual@onduex.com>
2022-02-15 12:36:41 +0100

    [IMP] set cost in invoice unactivated

--- imputations_to_sale/models/account_invoice.py
+++ imputations_to_sale/models/account_invoice.py
@@ -15,38 +15,38 @@ class AccountInvoice(models.Model):
         compute="_get_sale_order_from_invoice_line",
         help="Estos son todos los pedidos relacionados a la factura.")
 
-    @api.multi
-    def invoice_validate(self):
-        res = super(AccountInvoice, self).invoice_validate()
-        if res and self.type == "in_invoice":
-            self.set_product_cost()
-        return res
-
-    def set_product_cost(self):
-        """
-        Función para establecer el precio de coste a los productos de la
-        factura de compra.
-        :return: None
-        """
-        # OPERARIOS
-        operator_category_ids = self.get_operator_category()
-        operator_product_ids = self.env["product.template"].search([
-            ("categ_id", "in", operator_category_ids)]).mapped(
-            "product_variant_id")
-
-        # PUESTO DE TRABAJO
-        machine_product_ids = self.get_machine_category()
-        machine_product_ids = self.env["product.template"].search([
-            ("categ_id", "in", machine_product_ids)]).mapped(
-            "product_variant_id")
-
-        product_ids = operator_product_ids.ids + machine_product_ids.ids
-        line_ids = self.invoice_line_ids.filtered(
-            lambda x: x.product_id.id not in product_ids)
-
-        for line_id in line_ids:
-            line_id.product_id.write({"standard_price": line_id.price_unit})
-        return
+    # @api.multi
+    # def invoice_validate(self):
+    #     res = super(AccountInvoice, self).invoice_validate()
+    #     if res and self.type == "in_invoice":
+    #         self.set_product_cost()
+    #     return res
+    #
+    # def set_product_cost(self):
+    #     """
+    #     Función para establecer el precio de coste a los productos de la
+    #     factura de compra.
+    #     :return: None
+    #     """
+    #     # OPERARIOS
+    #     operator_category_ids = self.get_operator_category()
+    #     operator_product_ids = self.env["product.template"].search([
+    #         ("categ_id", "in", operator_category_ids)]).mapped(
+    #         "product_variant_id")
+    #
+    #     # PUESTO DE TRABAJO
+    #     machine_product_ids = self.get_machine_category()
+    #     machine_product_ids = self.env["product.template"].search([
+    #         ("categ_id", "in", machine_product_ids)]).mapped(
+    #         "product_variant_id")
+    #
+    #     product_ids = operator_product_ids.ids + machine_product_ids.ids
+    #     line_ids = self.invoice_line_ids.filtered(
+    #         lambda x: x.product_id.id not in product_ids)
+    #
+    #     for line_id in line_ids:
+    #         line_id.product_id.write({"standard_price": line_id.price_unit})
+    #     return
 
     def get_parent_operator_category(self):
         return self.env["product.category"].browse(769).id
